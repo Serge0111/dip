a1d3622f0d67a94790344c6f43fd16b282fb9fc5
User <user@Users-MacBook-Pro-2.local>
2018-12-11 05:02:48 +0200

    Validation

--- src/app/wrapper/wrapper.component.ts
+++ src/app/wrapper/wrapper.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-wrapper',
@@ -8,17 +8,57 @@ import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 })
 export class WrapperComponent implements OnInit {
   public form: FormGroup = this._fb.group({
-      start: [''],
-      end: [''],
-      date: ['']
-     });
-  public startDate = new Date(1990, 0, 1);
-     constructor(
-      private _fb: FormBuilder
-    ) { }
+    start: ['', [
+      Validators.required,
+      Validators.minLength(4),
+      this.forbiddenNameValidator(/[^а-я]{4,}/) // <-- Here's how you pass in the custom validator.
+    ]],
+    end: ['', [
+      Validators.required,
+      Validators.minLength(4),
+      this.forbiddenNameValidator(/[^а-я]{4,}/) // <-- Here's how you pass in the custom validator.
+    ]],
+    date: ['', [Validators.required, Validators.minLength(4)]]
+  });
+
+  public startvld = this.form.controls.start;
+  public endvld = this.form.controls.end;
+  public startDate: Date = new Date(1990, 0, 1);
+  public transport: string;
+  private start: string;
+  private end: string;
+  private date: string;
+  private status: string;
+  constructor(
+    private _fb: FormBuilder
+  ) { }
 
   ngOnInit() {
-
+    this.form.valueChanges.subscribe((data) => {
+      const { start, end, date } = data;
+      this.start = start;
+      this.end = end;
+      this.date = date;
+      this.status = this.form.status;
+    });
   }
 
+  public forbiddenNameValidator(nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? {'forbiddenName': {value: control.value}} : null;
+    };
+  }
+
+  public sendData(val) {
+    if (!this.transport) {
+      console.log('Choose transport type');
+    } else {
+      console.log(this.transport, this.status);
+    }
+  }
+  public handle(val) {
+    this.transport = val;
+    console.log(this.transport);
+  }
 }
